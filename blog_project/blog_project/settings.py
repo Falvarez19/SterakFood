64d0aa4b9ccec689156ce78daeae5c89f68befe5
Alvaro Alvarez <110843582+Falvarez19@users.noreply.github.com>
2024-11-22 20:55:47 -0300

    Update settings.py

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7n(*lgvr8fikxfhk_9(x9(i+wv#cnc+uvh5%lc8@ls$yao69x@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sterakfood.onrender.com']
 
 # formulario
 EEMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
