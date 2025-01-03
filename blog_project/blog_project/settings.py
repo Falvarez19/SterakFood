0dd28916aff7a2ea6cc75c8a22be60be9a2707af
alvaro <f.alvarezarg@gmail.com>
2025-01-03 17:53:01 -0300

    subiendo cambios para el despliegue y elimnando cosas que no servian como las imagenes de perfil predeter

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7n(*lgvr8fikxfhk_9(x9(i+wv#cnc+uvh5%lc8@ls$yao69x@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['sterakfood.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 # formulario
 EEMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
